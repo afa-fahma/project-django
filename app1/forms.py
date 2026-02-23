2c20976887534a4c31d053d383e9bb123168700d
afa-fahma <afalichu@gmail.com>
2026-02-23 20:25:01 +0530

    AddBook

--- app1/forms.py
+++ app1/forms.py
@@ -8,6 +8,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model=book
         fields=['title','author','price','pub_date','description']
+        widgets={
+            'title':forms.TextInput(attrs={'class':'form-control title-input'}),
+            'author':forms.TextInput(attrs={'class':'form-control author-input'}),
+            'price':forms.NumberInput(attrs={'class':'form-control price-input'}),
+            'pub_date':forms.DateInput(attrs={'class':'form-control pub_date-input'}),
+            'description':forms.Textarea(attrs={'class':'form-control description-input'}),
+            
+
+        }
 
 class userRegistrationform(UserCreationForm):
     class Meta:
